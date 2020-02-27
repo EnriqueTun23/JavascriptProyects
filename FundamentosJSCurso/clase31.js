74cd8e52791e8a8a5c78d623567f02062250fa06
EnriqueTun23 <quiquejesus94@gmail.com>
2020-02-27 11:21:53 -0600

    clase 35 terminado

---
+++ FundamentosJSCurso/clase31.js
@@ -0,0 +1,29 @@
+
+
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+
+const opts = { crossDomain: true}
+
+
+function obtenerPersonaje(id, callback) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(url, opts, callback).fail(function () {
+        console.log(`Sucedio un error. No se pudo obtener el personaje del _ID: ${id}`)
+    })
+}
+obtenerPersonaje(1, function(personaje){
+    console.log(`Hola, yo soy ${personaje.name}` )
+    obtenerPersonaje(2, function (personaje) {
+        console.log(`Hola, yo soy ${personaje.name}` )
+        obtenerPersonaje(3, function (personaje){
+            console.log(`Hola, yo soy ${personaje.name}` )
+            obtenerPersonaje(4, function (personaje) {
+                console.log(`Hola, yo soy ${personaje.name}` )
+                obtenerPersonaje(555, function(personaje){
+                    console.log(`Hola, yo soy ${personaje.name}` )
+                })
+            })
+        })
+    })
+})
